d5dad867e3e40b85561025de23f2697835f70a60
flamendless <flamendless8@gmail.com>
2025-06-20 07:33:42 +0800

    Script: Update run.sh

--- run.sh
+++ run.sh
@@ -7,19 +7,26 @@
 set -euf -o pipefail
 
 deps() {
-	go install github.com/alexkohler/prealloc@latest
-	go install github.com/nikolaydubina/smrcptr@latest
-	go install go.uber.org/nilaway/cmd/nilaway@latest
-	go install github.com/kisielk/errcheck@latest
+	go get -tool github.com/dkorunic/betteralign/cmd/betteralign@latest
+	go get -tool go.uber.org/nilaway/cmd/nilaway@latest
+	go get -tool github.com/alexkohler/prealloc@latest
+	go get -tool github.com/nikolaydubina/smrcptr@latest
+	go get -tool github.com/mdempsky/unconvert@latest
+	go get -tool github.com/kisielk/errcheck@latest
 }
 
-check() {
+sc() {
+	local -; set -x;
+	go fmt ./...
 	go mod tidy
 	go vet ./...
-	prealloc ./...
-	smrcptr ./...
-	nilaway ./...
-	errcheck ./...
+
+	go tool betteralign -apply ./...
+	go tool nilaway ./...
+	go tool prealloc ./...
+	go tool smrcptr ./...
+	go tool unconvert ./...
+	go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest -test ./...
 
 	set +f
 	local gofiles=( cmd/*.go src/**/*.go )
@@ -27,28 +34,35 @@ check() {
 		goimports -w -local -v "$file"
 	done
 	set -f
-}
 
-fmt() {
+	local PKGS
+	PKGS=$(go list ./... | tr "\n" " ")
+	go tool errcheck $PKGS
+
 	set +f
-	local gofiles=( src/**/*.go )
-	for file in "${gofiles[@]}"; do
-		goimports -w -local -v "$file"
+	local GODIRS
+	GODIRS=$(go list -f "{{.Dir}}" ./...)
+	for d in ${GODIRS}; do
+		go tool goimports -w -local -v "$d"/*.go
 	done
 	set -f
 }
 
-run() {
+runwin() {
 	GOOS=windows \
 		go build -o out/game.exe ./cmd/main.go \
 		&& cp out/game.exe /mnt/c/Users/flame/game.exe \
 		&& /mnt/c/Users/flame/game.exe --dev
 }
 
+run() {
+	GOOS=linux go run ./cmd/main.go --dev
+}
+
 if [ "$#" -eq 0 ]; then
 	echo "First use: chmod +x run.sh"
 	echo "Usage: ./run.sh run"
 else
-	echo "Running $@"
-	"$1" "$@"
+	echo "Running ${1}..."
+	time "$1" "$@"
 fi
