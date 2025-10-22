8e3bd1c5531ff3bd81b0af0d6f553f69be5b2629
flamendless <flamendless@gmail.com>
2025-10-22 14:34:22 +0800

    Feature: Create atlases and items structs

--- src/atlases/atlases.go
+++ src/atlases/atlases.go
@@ -2,16 +2,23 @@ package atlases
 
 import (
 	"remembering-home/src/common"
-	"remembering-home/src/dialogues"
 	"remembering-home/src/enums"
 )
 
-type Data struct {
-	Item      enums.Item
-	Name      string
-	Pos       common.Vec2
-	NoCol     bool
-	ReqColDir enums.Direction
-	Tags      []enums.ItemClass
-	Dialogue  dialogues.DialogueKeys
+type FrameData struct {
+	ID   enums.Item
+	Pos  common.Vec2
+	Size common.Vec2
+}
+
+type Metadata struct {
+	Padding   int
+	Extrude   int
+	Size      common.Vec2
+	QuadCount int
+}
+
+type AtlasData struct {
+	Frames   []FrameData
+	Metadata Metadata
 }
